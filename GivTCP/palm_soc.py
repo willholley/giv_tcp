42c425c030f752f1ae53be62722417acf51048c0
salewis38 <46199139+salewis38@users.noreply.github.com>
2023-08-02 17:26:22 +0100

    Corrected Solcast data length
    Previously reading past end of Solcast data due to code merge issue
    Also improved robustness for incomplete Solcast data

--- GivTCP/palm_soc.py
+++ GivTCP/palm_soc.py
@@ -593,9 +593,9 @@ class SolcastObj:
         pv_est90 = [0] * 10080
 
         if stgs.Solcast.url_sw != "":  # Two arrays are specified
-            forecast_lines = min(len(solcast_data_1['forecasts']), len(solcast_data_2['forecasts']))
+            forecast_lines = min(len(solcast_data_1['forecasts']), len(solcast_data_2['forecasts'])) - 1
         else:
-            forecast_lines = len(solcast_data_1['forecasts'])
+            forecast_lines = len(solcast_data_1['forecasts']) - 1
         interval = int(solcast_data_1['forecasts'][0]['period'][2:4])
         solcast_offset = (60 * int(solcast_data_1['forecasts'][0]['period_end'][11:13]) +
             int(solcast_data_1['forecasts'][0]['period_end'][14:16]) - interval - 60)
@@ -608,20 +608,23 @@ class SolcastObj:
         i = solcast_offset
         cntr = 0
         while i < solcast_offset + forecast_lines * interval:
-            if stgs.Solcast.url_sw != "":  # Two arrays are specified
-                pv_est10[i] = (int(solcast_data_1['forecasts'][cntr]['pv_estimate10'] * 1000) +
-                    int(solcast_data_2['forecasts'][cntr]['pv_estimate10'] * 1000))
-                pv_est50[i] = (int(solcast_data_1['forecasts'][cntr]['pv_estimate'] * 1000) +
-                    int(solcast_data_2['forecasts'][cntr]['pv_estimate'] * 1000))
-                pv_est90[i] = (int(solcast_data_1['forecasts'][cntr]['pv_estimate90'] * 1000) +
-                    int(solcast_data_2['forecasts'][cntr]['pv_estimate90'] * 1000))
-            else:
-                pv_est10[i] = int(solcast_data_1['forecasts'][cntr]['pv_estimate10'] * 1000)
-                pv_est50[i] = int(solcast_data_1['forecasts'][cntr]['pv_estimate'] * 1000)
-                pv_est90[i] = int(solcast_data_1['forecasts'][cntr]['pv_estimate90'] * 1000)
+            try:
+                if stgs.Solcast.url_sw != "":  # Two arrays are specified
+                    pv_est10[i] = (int(solcast_data_1['forecasts'][cntr]['pv_estimate10'] * 1000) +
+                        int(solcast_data_2['forecasts'][cntr]['pv_estimate10'] * 1000))
+                    pv_est50[i] = (int(solcast_data_1['forecasts'][cntr]['pv_estimate'] * 1000) +
+                        int(solcast_data_2['forecasts'][cntr]['pv_estimate'] * 1000))
+                    pv_est90[i] = (int(solcast_data_1['forecasts'][cntr]['pv_estimate90'] * 1000) +
+                        int(solcast_data_2['forecasts'][cntr]['pv_estimate90'] * 1000))
+                else:
+                    pv_est10[i] = int(solcast_data_1['forecasts'][cntr]['pv_estimate10'] * 1000)
+                    pv_est50[i] = int(solcast_data_1['forecasts'][cntr]['pv_estimate'] * 1000)
+                    pv_est90[i] = int(solcast_data_1['forecasts'][cntr]['pv_estimate90'] * 1000)
+            except:
+                break
 
             if i > 1 and i % interval == 0:
-                cntr += 1
+                    cntr += 1
             i += 1
 
         if solcast_offset > 720:  # Forget about current day
