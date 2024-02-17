c0dab17440decc2571e076a0db7237473a23bf10
danielgallo <daniel.gallo@gmail.com>
2024-02-17 18:32:44 +0000

    Add support for multiple inverters in WebDashboard

--- GivTCP/REST.py
+++ GivTCP/REST.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # version 2021.12.22
 from os.path import exists
-from flask import Flask, request, send_from_directory
+from flask import Flask, json as flask_json, request, send_from_directory
 from flask_cors import CORS
 import read as rd       #grab passthrough functions from main read file
 import write as wr      #grab passthrough functions from main write file
@@ -9,13 +9,14 @@ import evc as evc
 import config_dash as cfdash
 from GivLUT import GivQueue, GivLUT
 import os
-import json
+import json as std_json
 from settings import GiV_Settings
 
 logger = GivLUT.logger
 
 #set-up Flask details
 giv_api = Flask(__name__)
+jsonResponseType = { 'Content-Type': 'application/json; charset=utf-8' }
 CORS(giv_api)
 
 #Proxy Read Functions
@@ -35,95 +36,95 @@ def get_config_page():
 #Read from Invertor put in cache and publish
 @giv_api.route('/runAll', methods=['GET'])
 def getAll():
-    return rd.runAll(True)
+    return rd.runAll(True), 200, jsonResponseType
 
 @giv_api.route('/reboot', methods=['GET'])
 def reboot():
-    return wr.rebootinverter()
+    return wr.rebootinverter(), 200, jsonResponseType
 
 @giv_api.route('/restart', methods=['GET'])
 def restart():
-    return wr.rebootAddon()
+    return wr.rebootAddon(), 200, jsonResponseType
 
 #Publish last cached Invertor Data
 @giv_api.route('/readData', methods=['GET'])
 def rdData():
-    return rd.pubFromPickle()
+    return rd.pubFromPickle(), 200, jsonResponseType
 
 #Publish last cached Invertor Data
 @giv_api.route('/getCache', methods=['GET'])
 def gtCache():
-    return rd.getCache()
+    return rd.getCache(), 200, jsonResponseType
 
 # Read from Invertor put in cache
 @giv_api.route('/getData', methods=['GET'])
 def gtData():
-    return GivQueue.q.enqueue(rd.getData,True)
+    return GivQueue.q.enqueue(rd.getData,True), 200, jsonResponseType
 
 #Proxy Write Functions
 @giv_api.route('/enableChargeTarget', methods=['POST'])
 def enChargeTrgt():
     payload = request.get_json(silent=True, force=True)
-    return wr.enableChargeTarget(payload)
+    return wr.enableChargeTarget(payload), 200, jsonResponseType
 
 @giv_api.route('/enableChargeSchedule', methods=['POST'])
 def enableChrgSchedule():
     payload = request.get_json(silent=True, force=True)
-    return wr.enableChargeSchedule(payload)
+    return wr.enableChargeSchedule(payload), 200, jsonResponseType
 
 @giv_api.route('/enableDischargeSchedule', methods=['POST'])
 def enableDischrgSchedule():
     payload = request.get_json(silent=True, force=True)
-    return wr.enableDischargeSchedule(payload)
+    return wr.enableDischargeSchedule(payload), 200, jsonResponseType
 
 @giv_api.route('/enableDischarge', methods=['POST'])
 def enableBatDisharge():
     payload = request.get_json(silent=True, force=True)
-    return wr.enableDischarge(payload)
+    return wr.enableDischarge(payload), 200, jsonResponseType
 
 @giv_api.route('/setChargeTarget', methods=['POST'])
 def setChrgTarget():
     payload = request.get_json(silent=True, force=True)
-    return wr.setChargeTarget(payload)
+    return wr.setChargeTarget(payload), 200, jsonResponseType
 
 @giv_api.route('/setBatteryReserve', methods=['POST'])
 def setBattReserve():
     payload = request.get_json(silent=True, force=True)
-    return wr.setBatteryReserve(payload)
+    return wr.setBatteryReserve(payload), 200, jsonResponseType
 
 @giv_api.route('/setChargeRate', methods=['POST'])
 def setChrgeRate():
     payload = request.get_json(silent=True, force=True)
-    return wr.setChargeRate(payload)
+    return wr.setChargeRate(payload), 200, jsonResponseType
 
 @giv_api.route('/setDischargeRate', methods=['POST'])
 def setDischrgeRate():
     payload = request.get_json(silent=True, force=True)
-    return wr.setDischargeRate(payload)
+    return wr.setDischargeRate(payload), 200, jsonResponseType
 
 @giv_api.route('/setChargeSlot1', methods=['POST'])
 def setChrgSlot1():
     payload = request.get_json(silent=True, force=True)
     payload['slot']=1
-    return wr.setChargeSlot(payload)
+    return wr.setChargeSlot(payload), 200, jsonResponseType
 
 @giv_api.route('/setChargeSlot2', methods=['POST'])
 def setChrgSlot2():
     payload = request.get_json(silent=True, force=True)
     payload['slot']=2
-    return wr.setChargeSlot(payload)
+    return wr.setChargeSlot(payload), 200, jsonResponseType
 
 @giv_api.route('/setDischargeSlot1', methods=['POST'])
 def setDischrgSlot1():
     payload = request.get_json(silent=True, force=True)
     payload['slot']=1
-    return wr.setDischargeSlot(payload)
+    return wr.setDischargeSlot(payload), 200, jsonResponseType
 
 @giv_api.route('/setDischargeSlot2', methods=['POST'])
 def setDischrgSlot2():
     payload = request.get_json(silent=True, force=True)
     payload['slot']=2
-    return wr.setDischargeSlot(payload)
+    return wr.setDischargeSlot(payload), 200, jsonResponseType
 
 @giv_api.route('/tempPauseDischarge', methods=['POST'])
 def tmpPauseDischrg():
@@ -132,11 +133,11 @@ def tmpPauseDischrg():
         if exists(".tpdRunning"):
             jobid= str(open(".tpdRunning","r").readline())
             logger.critical("Retrieved jobID to cancel Temp Pause Discharge: "+ str(jobid))
-            return wr.cancelJob(jobid)
+            return wr.cancelJob(jobid), 200, jsonResponseType
         else:
             logger.error("Force Charge is not currently running")
     else:
-        return wr.tempPauseDischarge(payload)
+        return wr.tempPauseDischarge(payload), 200, jsonResponseType
 
 @giv_api.route('/tempPauseCharge', methods=['POST'])
 def tmpPauseChrg():
@@ -145,11 +146,11 @@ def tmpPauseChrg():
         if exists(".tpcRunning"):
             jobid= str(open(".tpcRunning","r").readline())
             logger.debug("Retrieved jobID to cancel Temp Pause Charge: "+ str(jobid))
-            return wr.cancelJob(jobid)
+            return wr.cancelJob(jobid), 200, jsonResponseType
         else:
             logger.error("Force Charge is not currently running")
     else:
-        return wr.tempPauseCharge(payload)
+        return wr.tempPauseCharge(payload), 200, jsonResponseType
 
 @giv_api.route('/forceCharge', methods=['POST'])
 def frceChrg():
@@ -159,10 +160,10 @@ def frceChrg():
         if exists(".FCRunning"):
             jobid= str(open(".FCRunning","r").readline())
             logger.debug("Retrieved jobID to cancel Force Charge: "+ str(jobid))
-            return wr.cancelJob(jobid)
+            return wr.cancelJob(jobid), 200, jsonResponseType
         else:
             logger.error("Force Charge is not currently running")
-    return wr.forceCharge(payload)
+    return wr.forceCharge(payload), 200, jsonResponseType
 
 @giv_api.route('/forceExport', methods=['POST'])
 def frceExprt():
@@ -171,79 +172,79 @@ def frceExprt():
         if exists(".FERunning"):
             jobid= str(open(".FERunning","r").readline())
             logger.debug("Retrieved jobID to cancel Force Export: "+ str(jobid))
-            return wr.cancelJob(jobid)
+            return wr.cancelJob(jobid), 200, jsonResponseType
         else:
             logger.error("Force Charge is not currently running")
-    return wr.forceExport(payload)
+    return wr.forceExport(payload), 200, jsonResponseType
 
 @giv_api.route('/setBatteryMode', methods=['POST'])
 def setBattMode():
     payload = request.get_json(silent=True, force=True)
-    return wr.setBatteryMode(payload)
+    return wr.setBatteryMode(payload), 200, jsonResponseType
 
 @giv_api.route('/setDateTime', methods=['POST'])
 def setDate():
     payload = request.get_json(silent=True, force=True)
-    return wr.setDateTime(payload)
+    return wr.setDateTime(payload), 200, jsonResponseType
 
 @giv_api.route('/switchRate', methods=['POST'])
 def swRates():
     payload = request.get_json(silent=True, force=True)
-    return wr.switchRate(payload)
+    return wr.switchRate(payload), 200, jsonResponseType
 
 @giv_api.route('/settings', methods=['GET'])
 def getFileData():
     file = open('/config/GivTCP/settings'+str(GiV_Settings.givtcp_instance)+'.json', 'r')
     #file = open(os.path.dirname(__file__) + '/settings.json', 'r')
-    data = json.load(file)
+    data = std_json.load(file)
     file.close()
     return data
 
 @giv_api.route('/settings', methods=['POST'])
 def editFileData():
     file = open('/config/GivTCP/settings'+str(GiV_Settings.givtcp_instance)+'.json', 'r')
-    data = json.load(file)
+    data = std_json.load(file)
     file.close()
     data.update(request.get_json(silent=True, force=True))
     file = open('/config/GivTCP/settings'+str(GiV_Settings.givtcp_instance)+'.json', 'w')
-    json.dump(data, file,indent=4)
+    std_json.dump(data, file,indent=4)
     file.close()
     return data
 
 @giv_api.route('/setImportCap', methods=['POST'])
 def impCap():
     payload = request.get_json(silent=True, force=True)
-    return evc.setImportCap(payload)
+    return evc.setImportCap(payload), 200, jsonResponseType
 
 @giv_api.route('/setCurrentLimit', methods=['POST'])
 def currLimit():
     payload = request.get_json(silent=True, force=True)
-    return evc.setCurrentLimit(payload)
+    return evc.setCurrentLimit(payload), 200, jsonResponseType
 
 @giv_api.route('/setChargeControl', methods=['POST'])
 def chrgeControl():
     payload = request.get_json(silent=True, force=True)
-    return evc.setChargeControl(payload)
+    return evc.setChargeControl(payload), 200, jsonResponseType
 
 @giv_api.route('/setChargeMode', methods=['POST'])
 def chrgMode():
     payload = request.get_json(silent=True, force=True)
-    return evc.setChargeMode(payload)
+    return evc.setChargeMode(payload), 200, jsonResponseType
 
 @giv_api.route('/setChargingMode', methods=['POST'])
 def chrgingMode():
     payload = request.get_json(silent=True, force=True)
-    return evc.setChargingMode(payload)
+    return evc.setChargingMode(payload), 200, jsonResponseType
 
 @giv_api.route('/setMaxSessionEnergy', methods=['POST'])
 def maxSession():
     payload = request.get_json(silent=True, force=True)
-    return evc.setMaxSessionEnergy(payload)
+    return evc.setMaxSessionEnergy(payload), 200, jsonResponseType
 
 @giv_api.route('/getEVCCache', methods=['GET'])
 def gtEVCChce():
     payload = request.get_json(silent=True, force=True)
-    return evc.getEVCCache()
+    return evc.getEVCCache(), 200, jsonResponseType
 
 
 
